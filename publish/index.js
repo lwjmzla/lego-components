127d72a4e4535dadc7a0f010b8ff6c372b030583
lwjmzla <214245088@qq.com>
2022-10-17 00:04:08 +0800

    update 1.0.9

--- publish/index.js
+++ publish/index.js
@@ -14,7 +14,7 @@ class InitCommand {
       chain = chain.then(() => this.init());
       chain = chain.then(() => this.exec());
       chain.catch((err) => {
-        console.error(err.message)
+        console.error(colors.red(err.message))
       })
     });
   }
@@ -22,6 +22,9 @@ class InitCommand {
   async init() {
     // !获取最新的版本号
     this.latestVersion = await getNpmLatestVersion(pkg.name, getDefaultRegistry())
+    if (!semver.gt(pkg.version, this.latestVersion)) {
+      throw new Error(`要发版的版本号需大于当前最新版本号${this.latestVersion}`)
+    }
   }
 
   async exec() {
@@ -46,47 +49,48 @@ class InitCommand {
   }
 
   async getComponentInfo () {
-    const versionCodes = this.latestVersion.split('.')
-    const newCode = parseInt(versionCodes.pop()) + 1
-    versionCodes.push(newCode)
-    const defaultVersion = versionCodes.join('.')
-    const latestVersion = this.latestVersion
+    // const versionCodes = this.latestVersion.split('.')
+    // const newCode = parseInt(versionCodes.pop()) + 1
+    // versionCodes.push(newCode)
+    // const defaultVersion = versionCodes.join('.')
+    // const latestVersion = this.latestVersion
 
-    const {version,isConfirm} = await inquirer.prompt([
-      {
-        type: 'input',
-        name: 'version',
-        message: `请输入版本号`,
-        default: defaultVersion,
-        validate: function (val) {
-          const done = this.async();
-          setTimeout(function() {
-            if (!semver.valid(val)) { // !返回 版本号  或者  null
-              done(`请输入合法的版本号`);
-              return;
-            }
-            if (!semver.gt(val, latestVersion)) {
-              done(`版本号需大于` + latestVersion);
-              return;
-            }
-            done(null, true);
-          }, 0);
-        },
-        // filter: (v) => semver.valid(v)  // ! v1.0.0 => 1.0.0，会改变最终值
-        // !当emver.valid(v)  返回null时，上面就报错，需要兼容处理
-        filter: (v) => {
-          return semver.valid(v) || v
-        }
-      },
+    const {isConfirm} = await inquirer.prompt([
+      // {
+      //   type: 'input',
+      //   name: 'version',
+      //   message: `请输入版本号`,
+      //   default: defaultVersion,
+      //   validate: function (val) {
+      //     const done = this.async();
+      //     setTimeout(function() {
+      //       if (!semver.valid(val)) { // !返回 版本号  或者  null
+      //         done(`请输入合法的版本号`);
+      //         return;
+      //       }
+      //       if (!semver.gt(val, latestVersion)) {
+      //         done(`版本号需大于` + latestVersion);
+      //         return;
+      //       }
+      //       done(null, true);
+      //     }, 0);
+      //   },
+      //   // filter: (v) => semver.valid(v)  // ! v1.0.0 => 1.0.0，会改变最终值
+      //   // !当emver.valid(v)  返回null时，上面就报错，需要兼容处理
+      //   filter: (v) => {
+      //     return semver.valid(v) || v
+      //   }
+      // },
       {
         type: 'confirm', // !二选一  true or false
         name: 'isConfirm',
-        message: 'are you sure?',
+        message: `当前需要发版的版本号为${pkg.version}, are you sure?`,
         default: false
       },
     ])
     return {
-      version,isConfirm
+      version: pkg.version,
+      isConfirm
     }
   }
 
